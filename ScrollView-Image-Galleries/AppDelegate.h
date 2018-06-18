1a8472d5c84e92efbc1dbdfa5083805bcb2f64b6
Erik Goossens <erikmgoossens@gmail.com>
2018-06-18 11:29:55 -0400

    Initial Commit

---
+++ ScrollView-Image-Galleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView-Image-Galleries
+//
+//  Created by Erik Goossens on 2018-06-18.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollView-Image-Galleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView-Image-Galleries
+//
+//  Created by Erik Goossens on 2018-06-18.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
